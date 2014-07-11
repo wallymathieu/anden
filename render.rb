a87873d37d4f2e9ae26d40ce55bff69c7d623146
Oskar Gewalli <gewalli@gmail.com>
2014-07-11 17:51:32 +0200

    Render as anden.sv.epub

--- render.rb
+++ render.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 file = File.join(File.dirname(__FILE__), *%w[text.sv.md])
-epub = File.join(File.dirname(__FILE__), *%w[text.sv.epub])
+epub = File.join(File.dirname(__FILE__), *%w[anden.sv.epub])
 meta = File.join(File.dirname(__FILE__), *%w[meta epub.xml])
 #pandoc
 system "pandoc #{file} -o #{epub} --epub-metadata=#{meta}" 
